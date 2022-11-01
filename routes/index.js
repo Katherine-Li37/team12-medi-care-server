36945d9a856116dbddf0a3f545ed27f1cdf68265
Catherine Li <catherine.cli37@gmail.com>
2022-10-31 23:07:13 -0500

    test 5

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,27 @@
 var express = require('express');
 var router = express.Router();
 // var passport = require('passport');
-var User = require('../models/user');
-var jwt = require('jsonwebtoken');
+// var User = require('../models/user');
+// var jwt = require('jsonwebtoken');
 
 router.get('/', function(req, res) {
     res.render('index', { user: req.user });
 });
 
-router.post('/register', function(req, res) {
-    User.register(new User({ 
-        username: req.body.username,
-        firstName: req.body.firstName ? req.body.firstName : null,
-        lastName: req.body.lastName ? req.body.lastName : null,
-        phone: req.body.phone ? req.body.phone : null,
-        status: 'active'
-    }), req.body.password, function(err, user) {
-        if (err) throw err;
-        // passport.authenticate('local')(req, res, function() {
-            res.send({message: 'User Created', user});
-        // });
-    });
-});
+// router.post('/register', function(req, res) {
+//     User.register(new User({ 
+//         username: req.body.username,
+//         firstName: req.body.firstName ? req.body.firstName : null,
+//         lastName: req.body.lastName ? req.body.lastName : null,
+//         phone: req.body.phone ? req.body.phone : null,
+//         status: 'active'
+//     }), req.body.password, function(err, user) {
+//         if (err) throw err;
+//         // passport.authenticate('local')(req, res, function() {
+//             res.send({message: 'User Created', user});
+//         // });
+//     });
+// });
 
 
 // router.post('/login', (req, res, next) => {
