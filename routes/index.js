4fd0df3513eb37faeb611bd615104ac9d2bcd85c
Catherine Li <catherine.cli37@gmail.com>
2022-10-31 23:05:27 -0500

    test 4

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+// var passport = require('passport');
 var User = require('../models/user');
 var jwt = require('jsonwebtoken');
 
@@ -17,39 +17,39 @@ router.post('/register', function(req, res) {
         status: 'active'
     }), req.body.password, function(err, user) {
         if (err) throw err;
-        passport.authenticate('local')(req, res, function() {
+        // passport.authenticate('local')(req, res, function() {
             res.send({message: 'User Created', user});
-        });
+        // });
     });
 });
 
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            res.json({ success: false, message: err })
-        } else {
-            if (!user) {
-                res.json({ success: false, message: 'Email or password incorrect' })
-            } else {
-                req.login(user, function(err) {
-                    if (err) {
-                        res.json({ success: false, message: err })
-                    } else {
-                        const token = jwt.sign({
-                            userId: user._id,
-                            username: user.username
-                        }, 'secret-token', { expiresIn: '24h' });
-                        res.json({ 
-                            success: true, 
-                            message: 'Authentication successful', 
-                            token: token, 
-                            username: user.username + ',' +  user._id});
-                    }
-                })
-            }
-        }
-    })(req, res);
-});
+// router.post('/login', (req, res, next) => {
+//     passport.authenticate('local', function(err, user, info) {
+//         if (err) {
+//             res.json({ success: false, message: err })
+//         } else {
+//             if (!user) {
+//                 res.json({ success: false, message: 'Email or password incorrect' })
+//             } else {
+//                 req.login(user, function(err) {
+//                     if (err) {
+//                         res.json({ success: false, message: err })
+//                     } else {
+//                         const token = jwt.sign({
+//                             userId: user._id,
+//                             username: user.username
+//                         }, 'secret-token', { expiresIn: '24h' });
+//                         res.json({ 
+//                             success: true, 
+//                             message: 'Authentication successful', 
+//                             token: token, 
+//                             username: user.username + ',' +  user._id});
+//                     }
+//                 })
+//             }
+//         }
+//     })(req, res);
+// });
 
 module.exports = router;
