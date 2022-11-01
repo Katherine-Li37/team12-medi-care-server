71dc19dee1a18d16a8e46bd1bd8aa4e71d0cba7f
Catherine Li <catherine.cli37@gmail.com>
2022-10-31 23:00:47 -0500

    test 2

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 require("dotenv").config({ path: "./config.env" });
 const express = require("express");
 const connectDB = require("./config/db");
-const postRoutes = require("./routes/postRoutes");
+const postRouter = require("./routes/postRoutes");
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+// const doctorDetailsRouter = require('./routes/doctor_details');
+const clinicsRouter = require('./routes/clinics');
+const appointmentsRouter = require('./routes/appointments');
 
 connectDB();
 
@@ -9,7 +14,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/api/v1/posts", postRoutes);
+app.use("/api/v1/posts", postRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+// app.use('/doctor_details', doctorDetailsRouter);
+app.use('/clinics', clinicsRouter);
+app.use('/appointments', appointmentsRouter);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
