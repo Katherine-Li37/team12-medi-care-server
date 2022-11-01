279c5bc64528738ba2aae8d94639fdbf7bcfa345
Catherine Li <catherine.cli37@gmail.com>
2022-11-01 16:24:02 -0500

    Fix log in and register api

--- routes/index.js
+++ routes/index.js
@@ -1,55 +1,93 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
-var User = require('../models/user');
+// var passport = require('passport');
+// var User = require('../models/user');
 var jwt = require('jsonwebtoken');
+var mongoose = require('mongoose');
+
+var monk = require('monk');
+var db = monk(process.env.MONGO_URI);
+
+var collection = db.get('users');
 
 router.get('/', function(req, res) {
     res.render('index', { user: req.user });
 });
 
 router.post('/register', function(req, res) {
-    User.register(new User({ 
+    collection.insert({
         username: req.body.username,
+        password: req.body.password,
         firstName: req.body.firstName ? req.body.firstName : null,
         lastName: req.body.lastName ? req.body.lastName : null,
         phone: req.body.phone ? req.body.phone : null,
         status: 'active'
-    }), req.body.password, function(err, user) {
-        if (err) throw err;
-        // passport.authenticate('local')(req, res, function() {
-            res.send({message: 'User Created', user});
-        // });
-    });
+    }, function(err, user) {
+        if (err) throw err
+        res.send({message: 'User Created', user});
+    })
 });
 
 
-router.post('/login', (req, res, next) => {
-    passport.authenticate('local', function(err, user, info) {
-        if (err) {
-            res.json({ success: false, message: err })
+// router.post('/register', function(req, res) {
+//     User.register(new User({ 
+//         username: req.body.username,
+//         firstName: req.body.firstName ? req.body.firstName : null,
+//         lastName: req.body.lastName ? req.body.lastName : null,
+//         phone: req.body.phone ? req.body.phone : null,
+//         status: 'active'
+//     }), req.body.password, function(err, user) {
+//         if (err) throw err;
+//         // passport.authenticate('local')(req, res, function() {
+//             res.send({message: 'User Created', user});
+//         // });
+//     });
+// });
+router.post('/login', function(req, res) {
+    collection.findOne({ 'username': req.body.username }, function(err, user) {
+        if (user.password === req.body.password){
+            const token = jwt.sign({
+                userId: user._id,
+                username: user.username
+            }, 'secret-token', { expiresIn: '24h' });
+            res.json({ 
+                success: true, 
+                message: 'Authentication successful', 
+                token: token, 
+                username: user.username + ',' +  user._id
+            });
         } else {
-            if (!user) {
-                res.json({ success: false, message: 'Email or password incorrect' })
-            } else {
-                req.login(user, function(err) {
-                    if (err) {
-                        res.json({ success: false, message: err })
-                    } else {
-                        const token = jwt.sign({
-                            userId: user._id,
-                            username: user.username
-                        }, 'secret-token', { expiresIn: '24h' });
-                        res.json({ 
-                            success: true, 
-                            message: 'Authentication successful', 
-                            token: token, 
-                            username: user.username + ',' +  user._id});
-                    }
-                })
-            }
+            res.json({ success: false, message: 'Email or password incorrect' })
         }
-    })(req, res);
+    })
 });
+
+// router.post('/login', (req, res, next) => {
+//     passport.authenticate('local', function(err, user, info) {
+//         if (err) {
+//             res.json({ success: false, message: err })
+//         } else {
+//             if (!user) {
+//                 res.json({ success: false, message: 'Email or password incorrect' })
+//             } else {
+//                 req.login(user, function(err) {
+//                     if (err) {
+//                         res.json({ success: false, message: err })
+//                     } else {
+//                         const token = jwt.sign({
+//                             userId: user._id,
+//                             username: user.username
+//                         }, 'secret-token', { expiresIn: '24h' });
+//                         res.json({ 
+//                             success: true, 
+//                             message: 'Authentication successful', 
+//                             token: token, 
+//                             username: user.username + ',' +  user._id});
+//                     }
+//                 })
+//             }
+//         }
+//     })(req, res);
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 var cors = require('cors');
-var cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+// var cookieParser = require('cookie-parser');
+// const bodyParser = require('body-parser');
+// var passport = require('passport');
+// var LocalStrategy = require('passport-local').Strategy;
 var session = require('express-session');
-var createError = require('http-errors');
-var User = require('./models/user');
+// var createError = require('http-errors');
+// var User = require('./models/user');
 //  End of Imports
 
 // ----------------- Driver Code --------------------------------------------
@@ -33,13 +33,13 @@ app.use(
     })
 );
 app.use(session({ secret: 'this-is-a-secret-token' }));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 // passport config
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-passport.use(new LocalStrategy(User.authenticate()));
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// passport.use(new LocalStrategy(User.authenticate()));
 
 
 const indexRouter = require('./routes/index');
