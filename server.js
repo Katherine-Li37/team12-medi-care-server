0094470b60b86e6c581fcdc30224218564134857
Katherine-Li37 <catherine.cli37@gmail.com>
2022-11-02 19:47:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: ['https://team12-medi-care.surge.sh/', 'http://localhost:3006', 'http://localhost:3006'],
+        origin: ['https://team12-medi-care.surge.sh/', 'http://localhost:3006', 'http://localhost:3000'],
         credentials: true,
     })
 );
