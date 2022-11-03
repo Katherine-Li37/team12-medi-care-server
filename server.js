93fc51a660da7ff9aa3cc834a19aa5aff4d840ff
Katherine-Li37 <catherine.cli37@gmail.com>
2022-11-02 19:31:20 -0500

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
     cors({
-        origin: 'https://katherine-li37.github.io', // <-- location of the react app were connecting to
+        origin: '*',
         credentials: true,
     })
 );
