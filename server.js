9cf137a48a025c8af2e0620b8104d5679edaa489
Catherine Li <catherine.cli37@gmail.com>
2022-11-09 19:54:50 -0600

    18

--- server.js
+++ server.js
@@ -32,12 +32,12 @@ app.use(express.urlencoded({ extended: true }));
 //     policy: 'no-referrer' // Compliant
 //   })
 // );
-app.use(
-    cors({
-        origin: [, 'http://localhost:3000'],
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: [, 'http://localhost:3000'],
+//         credentials: true,
+//     })
+// );
 // app.use((req, res, next) => {
 //   res.header("Access-Control-Allow-Origin", '*');
 //   res.header("Access-Control-Allow-Credentials", true);
